8b7923cbe6297842fcaae1218ac85c69182db169
dann <austindan168@gmail.com>
2024-11-14 09:23:27 +0300

    footer

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,9 @@
 function About(){
     return(
         <div id="about">
-          <h1>About</h1>       
+          <h1>About</h1>
+          <p>We are loacted in Nairobi Ngong Lane next to Quivers Club.
+             Together we can succeed, let us make it our mission to save marine life and live in  a fresh natural world. Together we can.</p>       
         </div>
     )
 }
